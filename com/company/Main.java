dbd976ce11fe95b56e7df04ba5ba2a20fcd0d43c
TKuhner <71107536+TKuhner@users.noreply.github.com>
2020-12-07 20:00:37 -0600

    Add files via upload

---
+++ com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Board myBoard = new Board();
+
+        myBoard.showBoard();
+
+
+        myBoard.saveBoard();
+
+
+        myBoard.loadBoard();
+    }
+}
